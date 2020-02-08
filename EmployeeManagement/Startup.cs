837e4b0f1354b41c4b86289484ab417df5a49ade
bosnjaknikola <nikola.bosnjak@wayseven.com>
2020-02-08 08:47:21 +0100

    initial commit

---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Only points to default html
+            app.UseDefaultFiles();
+
+            //serves static files from root (defaualt html) cant have this before use default files
+            app.UseStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello world");
+            });
+
+
+        }
+    }
+}
